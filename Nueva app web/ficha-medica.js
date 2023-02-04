19499798e02ec0304330347529db7787f5b73b00
Stiweard Jesús Ramírez Cortez <109332844+Stiweard@users.noreply.github.com>
2023-02-04 19:51:31 -0400

    Add files via upload

---
+++ Nueva app web/ficha-medica.js
@@ -0,0 +1,77 @@
+const songList = [
+    {
+        title: "Estado de salud"
+    },
+    {
+        title: "Mediciones y metas"
+    },
+    {
+        title: "Agregar otros alimentos"
+    },
+    {
+        title: "Resumen semanal"
+    },
+    {
+        title: "Auditorias"
+    },
+    {
+        title: "Reprogramacion"
+    },
+    {
+        title: "Actitudes"
+    },
+    {
+        title: "Medio ambiente"
+    },
+    {
+        title: "Adcciones"
+    },
+    {
+        title: "Psi(flores)"
+    },
+    {
+        title: "Alimentos f1"
+    },
+    {
+        title: "Alimentos f2"
+    },
+    {
+        title: "Alimentos f3"
+    },
+    {
+        title: "Ejercicios"
+    },
+    {
+        title: "Fuerza mv"
+    },
+    {
+        title: "Cardio mv"
+    },
+    {
+        title: "Sintomas"
+    },
+    {
+        title: "Órganos"
+    },
+    {
+        title: "Enfermedades"
+    },
+    {
+        title: "Tratamientos"
+    },
+    {
+        title: "Estudios pendientas"
+    },
+    {
+        title: "Interconsultas"
+    },
+    {
+        title: "Administración"
+    },
+    {
+        title: "Material utilizado"
+
+    },
+]
+
+console.log(songList)
---
+++ Nueva app web/search.js
@@ -0,0 +1,29 @@
+let search = document.querySelector('.search')
+let close = document.querySelector('.close')
+let searchBox = document.querySelector('.searchBox')
+
+search.onclick = function(){
+    searchBox.classList.add('active')
+}
+
+close.onclick = function(){
+    searchBox.classList.remove('active')
+}
+
+$(document).ready(function () {
+    $('#buscador').keyup(function () {
+        var nombres = $('.nombres');
+        var buscando = $(this).val();
+        var item = '';
+        for (var i = 0; i < nombres.length; i++) {
+            item = $(nombres[i]).html();
+            for (var x = 0; x < item.length; x++) {
+                if (buscando.length == 0 || item.indexOf(buscando) > -1) {
+                    $(nombres[i]).parents('.item').show();
+                } else {
+                    $(nombres[i]).parents('.item').hide();
+                }
+            }
+        }
+    });
+});
