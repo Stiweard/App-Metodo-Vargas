78d95b35d396811c19a0403fbe93d46e293b1b72
Stiweard Jesús Ramírez Cortez <109332844+Stiweard@users.noreply.github.com>
2023-02-13 20:58:56 -0400

    circulos y su valor

--- graficos.js
+++ graficos.js
@@ -131,47 +131,122 @@ function crearGraficos1(divContenedorGr, valoresGr1, nombrevalor1, colorGr) {
     });
 }
 
-function crearCirculosPro() {
+function crearCirculosPro(valordelcirculoadar,) {
 
     var arryGeneC = [];
-    arryGeneC.push(arrayInfoGeneral.registros.calificacion.general)
+    arryGeneC.push(valordelcirculoadar)
 
     var arryGeneC1 = arryGeneC[0];
 
 
     console.log(arryGeneC1)
+    console.log(xValues)
 
-    arryGeneC1.forEach((e) => {
-        if (valoresGr1[e] == undefined) {
-            arrGrasa.push(0);
-        } else {
-            arrGrasa.push(valoresGr1[e]);
+    xValues.forEach((e) => {
+        if (valordelcirculoadar[e] == undefined) {
+            var contenedorCirculosP = document.querySelector('.contenedorCirculos')
+            var divCp = document.createElement("div");
+            divCp.classList.add("divsemanaAndCirculo");
+            var PcirculosP = document.createElement("p");
+            var divcirculo = document.createElement("div");
+            divcirculo.classList.add("divValorMG");
+
+            divcirculo.style.background = 'rgb(63, 63, 63)'
+
+            PcirculosP.textContent = e
+            divCp.appendChild(PcirculosP);
+            divCp.appendChild(divcirculo);
+            contenedorCirculosP.appendChild(divCp);
+
+
+        } else if (valordelcirculoadar[e] == 1) {
+            var contenedorCirculosP = document.querySelector('.contenedorCirculos')
+            var divCp = document.createElement("div");
+            divCp.classList.add("divsemanaAndCirculo");
+            var PcirculosP = document.createElement("p");
+            var divcirculo = document.createElement("div");
+            divcirculo.classList.add("divValorMG");
+
+            divcirculo.style.background = 'rgb(244, 67, 54)'
+
+            PcirculosP.textContent = e
+            divCp.appendChild(PcirculosP);
+            divCp.appendChild(divcirculo);
+            contenedorCirculosP.appendChild(divCp);
+
+        } else if (valordelcirculoadar[e] == 2) {
+            var contenedorCirculosP = document.querySelector('.contenedorCirculos')
+            var divCp = document.createElement("div");
+            divCp.classList.add("divsemanaAndCirculo");
+            var PcirculosP = document.createElement("p");
+            var divcirculo = document.createElement("div");
+            divcirculo.classList.add("divValorMG");
+
+            divcirculo.style.background = 'rgb(255, 152, 0)'
+
+            PcirculosP.textContent = e
+            divCp.appendChild(PcirculosP);
+            divCp.appendChild(divcirculo);
+            contenedorCirculosP.appendChild(divCp);
+        } else if (valordelcirculoadar[e] == 3) {
+            var contenedorCirculosP = document.querySelector('.contenedorCirculos')
+            var divCp = document.createElement("div");
+            divCp.classList.add("divsemanaAndCirculo");
+            var PcirculosP = document.createElement("p");
+            var divcirculo = document.createElement("div");
+            divcirculo.classList.add("divValorMG");
+
+            divcirculo.style.background = 'rgb(255, 235, 59)'
+
+            PcirculosP.textContent = e
+            divCp.appendChild(PcirculosP);
+            divCp.appendChild(divcirculo);
+            contenedorCirculosP.appendChild(divCp);
+        } else if (valordelcirculoadar[e] == 4) {
+            var contenedorCirculosP = document.querySelector('.contenedorCirculos')
+            var divCp = document.createElement("div");
+            divCp.classList.add("divsemanaAndCirculo");
+            var PcirculosP = document.createElement("p");
+            var divcirculo = document.createElement("div");
+            divcirculo.classList.add("divValorMG");
+
+            divcirculo.style.background = 'rgb(173, 255, 47)'
+
+            PcirculosP.textContent = e
+            divCp.appendChild(PcirculosP);
+            divCp.appendChild(divcirculo);
+            contenedorCirculosP.appendChild(divCp);
+        } else if (valordelcirculoadar[e] == 5) {
+            var contenedorCirculosP = document.querySelector('.contenedorCirculos')
+            var divCp = document.createElement("div");
+            divCp.classList.add("divsemanaAndCirculo");
+            var PcirculosP = document.createElement("p");
+            var divcirculo = document.createElement("div");
+            divcirculo.classList.add("divValorMG");
+
+            divcirculo.style.background = 'rgb(24, 174, 255)'
+
+            PcirculosP.textContent = e
+            divCp.appendChild(PcirculosP);
+            divCp.appendChild(divcirculo);
+            contenedorCirculosP.appendChild(divCp);
         }
     });
 }
-crearCirculosPro()
+
+
+
 
 document.querySelector("body").addEventListener("click", (e) => {
     console.log(e.target.className);
     if (e.target.className == "fa-solid fa-chevron-right selectorFlecha1") {
-        crearGraficos1(
-            ".div-graficoAvanceSemanal",
-            arrayInfoGeneral.registros.calificacion.general,
-            "General",
-            "#3effff"
-        );
-    } else if (e.target.className == "fa-solid fa-chevron-right selectorFlecha2")
-        crearGraficos1(
-            ".div-graficoAvanceSemanal",
-            arrayInfoGeneral.registros.calificacion.avance,
-            "Avance",
-            "#1f3ff3"
-        );
+        crearGraficos1(".div-graficoAvanceSemanal", arrayInfoGeneral.registros.calificacion.general, "General", "#3effff");
+        crearCirculosPro(arrayInfoGeneral.registros.calificacion.general)
+
+    } else if (e.target.className == "fa-solid fa-chevron-right selectorFlecha2"){
+        crearGraficos1(".div-graficoAvanceSemanal", arrayInfoGeneral.registros.calificacion.avance, "Avance", "#1f3ff3");
+        crearCirculosPro(arrayInfoGeneral.registros.calificacion.avance)
+    }
 });
-crearGraficos(
-    ".div-grafico-peso-grasa",
-    arrayInfoGeneral.registros.mediciones.peso,
-    arrayInfoGeneral.registros.mediciones.grasa,
-    "Grasa",
-    "Peso"
-);
+
+crearGraficos(".div-grafico-peso-grasa",arrayInfoGeneral.registros.mediciones.peso,arrayInfoGeneral.registros.mediciones.grasa,"Grasa","Peso");
